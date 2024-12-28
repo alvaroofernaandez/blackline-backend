9452dc19cad11a66b62712e29787a59d2c7bc671
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-12-28 03:27:31 +0100

    avances modelos citas y diseños

--- api-principal/usuarios/api/Models/DiseñoModel.py
+++ api-principal/usuarios/api/Models/DiseñoModel.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from django.db import models
 
 class Design(models.Model):
-    image = models.ImageField(upload_to='designs/', blank=False, verbose_name="Imagen del diseño")
+    image = models.ImageField(upload_to='designs/', blank=False)
     titulo = models.CharField(blank=False, max_length=150)
     descripcion = models.CharField(blank=False, max_length=500)
     precio = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)

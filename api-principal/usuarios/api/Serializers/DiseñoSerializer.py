58ac0a3a6a0533668999b88c93b617e1a288ac84
alvaroofernaandez <alvaroofernaandez@gmail.com>
2025-01-11 21:43:35 +0100

    Agregar serializer y migración para el modelo Diseño en la API de usuarios

---
+++ api-principal/usuarios/api/Serializers/DiseñoSerializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from ..Models.DiseñoModel import Design
+
+class DiseñoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Design
+        fields = '__all__'

6132b2adc85d75af1a69e79b5ac2b2d42f6e9486
juan <jdeomoya@gmail.com>
2025-01-11 17:04:13 +0100

    requirements.txt para sorteos actualizado

--- sorteos-api/sorteos/sorteos/settings.py
+++ sorteos-api/sorteos/sorteos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from mongoengine import connect
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +81,6 @@ DATABASES = {
     }
 }
 
-from mongoengine import connect
 connect('sorteos', host='localhost', port=27017)
 
 print("Conexión exitosa")

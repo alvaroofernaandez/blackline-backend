9f6a13e6b5cd75a53899da5281e5f284d54657f4
juan <jdeomoya@gmail.com>
2025-03-31 21:09:16 +0200

    Cambios en la estructura y los nombres de la api gateway

--- api-gateway/apigateway/api/urls.py
+++ api-gateway/apigateway/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    # Ruta para capturar 'service_name' y 'sorteo_id' sin el prefijo 'api'
-    re_path(r'^(?P<service_name>[\w-]+)/(?P<sorteo_id>[\w-]+)/$', views.route_request, name='route_request_with_id'),
+    # Ruta para capturar 'service_name' y 'id'
+    re_path(r'^(?P<service_name>[\w-]+)/(?P<id>[\w-]+)/$', views.route_request, name='route_request_with_id'),
 
-    # Ruta para capturar solo 'service_name' sin 'sorteo_id' y sin el prefijo 'api'
+    # Ruta para capturar solo 'service_name' sin 'id'
     re_path(r'^(?P<service_name>[\w-]+)/$', views.route_request, name='route_request_without_id'),
 ]

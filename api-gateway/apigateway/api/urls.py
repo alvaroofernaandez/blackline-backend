26cb9e74d05847f9a8f280772b7cc1a0ce2a736c
alvaroofernaandez <alvaroofernaandez@gmail.com>
2025-04-29 11:40:42 +0200

    Añadida ruta para capturar 'extra' en la URL de la API

--- api-gateway/apigateway/api/urls.py
+++ api-gateway/apigateway/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    # Ruta para capturar 'service_name' y 'id'
+    re_path(r'^(?P<service_name>[\w-]+)/(?P<id>[\w-]+)/(?P<extra>[\w-]+)/$', views.route_request, name='route_request_with_extra'),
+
     re_path(r'^(?P<service_name>[\w-]+)/(?P<id>[\w-]+)/$', views.route_request, name='route_request_with_id'),
 
-    # Ruta para capturar solo 'service_name' sin 'id'
     re_path(r'^(?P<service_name>[\w-]+)/$', views.route_request, name='route_request_without_id'),
 ]

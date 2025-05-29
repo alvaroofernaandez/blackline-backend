5d35045a8fe87a8def605695f4a4ba8bd726d95d
alvaroofernaandez <alvaroofernaandez@gmail.com>
2025-05-29 16:23:43 +0200

    fix: permitir que el campo de imagen en el modelo Design acepte valores nulos y actualizar el serializer para manejar la imagen correctamente

--- api-principal/usuarios/api/Models/DiseñoModel.py
+++ api-principal/usuarios/api/Models/DiseñoModel.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Design(models.Model):
-    image = models.ImageField(blank=False, upload_to='diseños/')
+    image = models.ImageField(blank=False, upload_to='diseños/', null=True, blank=True)
     titulo = models.CharField(blank=False, max_length=150)
     descripcion = models.CharField(blank=False, max_length=500)
     precio = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
--- api-principal/usuarios/api/Serializers/DiseñoSerializer.py
+++ api-principal/usuarios/api/Serializers/DiseñoSerializer.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-
 from ..Models.DiseñoModel import Design
-
 
 class DiseñoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Design
         fields = '__all__'
+
+    def update(self, instance, validated_data):
+        imagen_file = validated_data.pop('imagen', None)
+
+        if imagen_file:
+            instance.imagen = imagen_file  
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance

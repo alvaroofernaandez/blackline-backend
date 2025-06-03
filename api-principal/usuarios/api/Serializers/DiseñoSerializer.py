415f6e439838b9654b3f7153538b15744175c3ae
alvaroofernaandez <alvaroofernaandez@gmail.com>
2025-06-03 13:03:51 +0200

    fix: corregir el nombre del campo 'image' en DiseñoSerializer y limpiar comentarios en views.py

--- api-principal/usuarios/api/Serializers/DiseñoSerializer.py
+++ api-principal/usuarios/api/Serializers/DiseñoSerializer.py
@@ -7,10 +7,10 @@ class DiseñoSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def update(self, instance, validated_data):
-        imagen_file = validated_data.pop('imagen', None)
+        imagen_file = validated_data.pop('image', None)
 
-        if imagen_file:
-            instance.imagen = imagen_file  
+        if imagen_file is not None:
+            instance.image = imagen_file
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)

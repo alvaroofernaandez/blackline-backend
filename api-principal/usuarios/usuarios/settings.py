269ffb809415e33df7217643c6cebf0e81c45659
juan <jdeomoya@gmail.com>
2025-06-10 11:52:59 +0200

    fix: campos del html de facturas cambiados y la expiracion del token ahora es cada 2 días en vez de 7

--- api-principal/usuarios/usuarios/settings.py
+++ api-principal/usuarios/usuarios/settings.py
@@ -136,8 +136,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=2),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
